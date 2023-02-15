6054a31645ecb766dae7c10172e28cadc99f2e8e
Малыгин Стас <s.maligin939@gmail.com>
2023-02-15 18:22:37 +0300

    Primary_Code

---
+++ app/src/main/java/com/example/chatandmapv2_0/ChatActivity.java
@@ -0,0 +1,14 @@
+package com.example.chatandmapv2_0;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ChatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+    }
+}
